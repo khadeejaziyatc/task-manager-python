f1b29201ec1f0fcfc0c76ef55187e502efc692e4
khadeejaziyatc <146641517+khadeejaziyatc@users.noreply.github.com>
2026-04-08 18:49:48 +0530

    Add task deletion functionality to task manager

--- task_manager.py
+++ task_manager.py
@@ -1,9 +1,11 @@
+
 tasks = []
 
 while True:
     print("\n1. Add Task")
     print("2. Show Tasks")
-    print("3. Exit")
+    print("3. Delete Task")
+    print("4. Exit")
 
     choice = input("Enter choice: ")
 
@@ -19,6 +21,20 @@ while True:
                 print(i + 1, ".", tasks[i])
 
     elif choice == "3":
+        if len(tasks) == 0:
+            print("No tasks to delete")
+        else:
+            for i in range(len(tasks)):
+                print(i + 1, ".", tasks[i])
+
+            num = int(input("Enter task number to delete: "))
+            if 1 <= num <= len(tasks):
+                tasks.pop(num - 1)
+                print("Task deleted")
+            else:
+                print("Invalid number")
+
+    elif choice == "4":
         break
 
     else:
