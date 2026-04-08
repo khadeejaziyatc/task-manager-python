69659d7a3787e26e3e6f3346edbe5a8d84559205
khadeejaziyatc <146641517+khadeejaziyatc@users.noreply.github.com>
2026-04-08 18:28:54 +0530

    Update task_manager.py

--- task_manager.py
+++ task_manager.py
@@ -1,14 +1,25 @@
-def show_tasks(tasks):
-    print("\n--- Your Task List ---")
-    for i in range(len(tasks)):
-        print(i + 1, ".", tasks[i])
-
-
-n = int(input("How many tasks: "))
 tasks = []
 
-for i in range(n):
-    task = input("Enter task: ")
-    tasks.append(task)
+while True:
+    print("\n1. Add Task")
+    print("2. Show Tasks")
+    print("3. Exit")
 
-show_tasks(tasks)
+    choice = input("Enter choice: ")
+
+    if choice == "1":
+        task = input("Enter task: ")
+        tasks.append(task)
+
+    elif choice == "2":
+        if len(tasks) == 0:
+            print("No tasks yet")
+        else:
+            for i in range(len(tasks)):
+                print(i + 1, ".", tasks[i])
+
+    elif choice == "3":
+        break
+
+    else:
+        print("Invalid choice")
